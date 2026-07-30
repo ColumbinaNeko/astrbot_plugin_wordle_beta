c3f90cf121b46714a930018a6f52214353d1fe48
ColumbinaNeko <columbinaneko@users.noreply.github.com>
2026-07-31 01:27:19 +0800

    feat: 每日配色改为珊瑚/青蓝 (#e8734a / #3da5a0)

--- wordle.py
+++ wordle.py
@@ -28,9 +28,9 @@ class Wordle:
     # 默认配色（绿 / 黄 / 灰）
     CORRECT_COLOR: ClassVar[tuple[int, int, int]] = (134, 163, 115)
     EXIST_COLOR: ClassVar[tuple[int, int, int]] = (198, 182, 109)
-    # 每日模式配色（紫 / 橙 / 灰）
-    DAILY_CORRECT_COLOR: ClassVar[tuple[int, int, int]] = (73, 52, 90)
-    DAILY_EXIST_COLOR: ClassVar[tuple[int, int, int]] = (248, 120, 24)
+    # 每日模式配色（珊瑚 / 青蓝 / 灰）
+    DAILY_CORRECT_COLOR: ClassVar[tuple[int, int, int]] = (232, 115, 74)
+    DAILY_EXIST_COLOR: ClassVar[tuple[int, int, int]] = (61, 165, 160)
 
     WRONG_COLOR: ClassVar[tuple[int, int, int]] = (123, 123, 124)
     BORDER_COLOR: ClassVar[tuple[int, int, int]] = (123, 123, 124)
