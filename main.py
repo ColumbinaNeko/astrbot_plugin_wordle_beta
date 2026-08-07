6af4bdaa0c787d3c0b3a1fd13fd451cf9060eee9
ColumbinaNeko <columbinaneko@users.noreply.github.com>
2026-08-07 20:15:42 +0800

    style: 结束消息加结果符号

--- main.py
+++ main.py
@@ -234,7 +234,7 @@ class WordlePlugin(Star):
             else:
                 await self._stop_game(session_id)
             yield event.chain_result(
-                [image_comp, Comp.Plain(f"卧槽！不愧是你~\n{game.result}")]
+                [image_comp, Comp.Plain(f"🎉 卧槽！不愧是你~\n{game.result}")]
             )
         elif guess_result == GuessResult.LOSS:
             if is_daily:
@@ -242,7 +242,7 @@ class WordlePlugin(Star):
             else:
                 await self._stop_game(session_id)
             yield event.chain_result(
-                [image_comp, Comp.Plain(f"很遗憾，这就是结局.\n{game.result}")]
+                [image_comp, Comp.Plain(f"❌ 很遗憾，这就是结局.\n{game.result}")]
             )
         else:
             remaining = game.rows - len(game.guessed_words)
